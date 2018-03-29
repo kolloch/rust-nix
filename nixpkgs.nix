c5675eeb58d4a79adad1a89cea806a8e1808e485
Peter Kolloch <info@eigenvalue.net>
2018-03-29 23:27:18 +0200

    With mozilla overlay and a newer rustc, things break down

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -7,12 +7,24 @@ let
     rev = "3410d73b2012aa429981d598993e0210cb68beb1";
     sha256 = "16h1m2myjlzm320m71w7bk958graxlbvhq5rxrx9gv1vaawwcw8g";
   };
-  moz_overlay = import (
-    builtins.fetchTarball 
-      https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz );
+  moz_pkgs = builtins.fetchTarball 
+      https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz;
+  moz_overlay = import moz_pkgs;
   in (import fetchNixpkgs { 
     config = {};
     overlays = [
-      moz_overlay
+        moz_overlay
+
+        (self: super:
+        let
+        # base = super.rustChannels.nightly;
+        base = super.rustChannelOf { channel = "stable"; };
+        in
+        {
+        rust = {
+            rustc = base.rust;
+            cargo = base.cargo;
+        };
+        })
     ];
   })
