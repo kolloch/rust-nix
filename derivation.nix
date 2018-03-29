730da5fe0df0215107596bfb486a7e29e7f9dffc
Peter Kolloch <info@eigenvalue.net>
2018-03-29 23:13:12 +0200

    Using ripgrep from 17.09 branch with additional echo to force rebuild.

--- derivation.nix
+++ derivation.nix
@@ -1,16 +1,31 @@
-{ pkgs, rustPlatform, stdenv, fetchFromGitHub }:
+{ stdenv, fetchFromGitHub, rustPlatform, makeWrapper }:
 
 with rustPlatform;
 
 buildRustPackage rec {
-  name = "hello-${version}";
-  version = "0.4.0";
+  name = "ripgrep-${version}";
+  version = "0.6.0";
 
-  buildInputs = with pkgs; [
-      openssl
-  ];
+  src = fetchFromGitHub {
+    owner = "BurntSushi";
+    repo = "ripgrep";
+    rev = "${version}";
+    sha256 = "1cnvwxbznmsn1gand8hhy5zadax5p67lvm46fkj1a1s89f158w3a";
+  };
 
-  src = ./rust;
+  depsSha256 = "1kx9xazhj93xa3cnys39wwr84qqjqrlsbbi5ih71vxppskdpvd6m";
 
-  depsSha256 = "";
+  preFixup = ''
+    echo "change"
+    mkdir -p "$out/man/man1"
+    cp "$src/doc/rg.1" "$out/man/man1"
+  '';
+
+  meta = with stdenv.lib; {
+    description = "A utility that combines the usability of The Silver Searcher with the raw speed of grep";
+    homepage = https://github.com/BurntSushi/ripgrep;
+    license = with licenses; [ unlicense /* or */ mit ];
+    maintainers = [ maintainers.tailhook ];
+    platforms = platforms.all;
+  };
 }
